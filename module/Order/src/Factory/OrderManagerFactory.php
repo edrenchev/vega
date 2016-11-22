05194407b814355a5d74873747923af647406899
Ervin <edrenchev@gmail.com>
2016-11-22 10:28:16 +0200

    Bug fix - order session key

--- module/Order/src/Factory/OrderManagerFactory.php
+++ module/Order/src/Factory/OrderManagerFactory.php
@@ -10,7 +10,7 @@ class OrderManagerFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $entityManager = $container->get('doctrine.entitymanager.orm_default');
-		$sessionSearchOrderForm = $container->get('SearchClientForm');
+		$sessionSearchOrderForm = $container->get('SearchOrderForm');
 
         return new OrderManager($entityManager, $sessionSearchOrderForm);
     }

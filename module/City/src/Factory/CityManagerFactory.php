14a803016ac9c8c1cd7055844594c5235d97c29d
Ervin <edrenchev@gmail.com>
2016-11-22 10:28:05 +0200

    Bug fix - city session key

--- module/City/src/Factory/CityManagerFactory.php
+++ module/City/src/Factory/CityManagerFactory.php
@@ -10,7 +10,7 @@ class CityManagerFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $entityManager = $container->get('doctrine.entitymanager.orm_default');
-		$sessionSearchCityForm = $container->get('SearchClientForm');
+		$sessionSearchCityForm = $container->get('SearchCityForm');
 
         return new CityManager($entityManager, $sessionSearchCityForm);
     }

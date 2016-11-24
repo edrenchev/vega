d2e0d2b9186cbd9472dc9877d7cfc41ea33925e5
Ervin <edrenchev@gmail.com>
2016-11-24 17:24:10 +0200

    Add error_log DB table

--- SQLChange/2016-11-24.sql
+++ SQLChange/2016-11-24.sql
@@ -1,3 +1,21 @@
 ALTER TABLE `clients` ADD `name` VARCHAR(255) NOT NULL AFTER `id`;
 
 UPDATE clients SET name = CONCAT_WS(' ', NULLIF(first_name, ''), NULLIF(middle_name, ''), NULLIF(last_name, ''));
+
+CREATE TABLE IF NOT EXISTS `error_log` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `creation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `reference` varchar(6) DEFAULT '',
+  `type` varchar(10) DEFAULT 'ERROR',
+  `priority` varchar(6) DEFAULT 'DEBUG',
+  `message` text,
+  `file` text,
+  `line` varchar(12),
+  `trace` text,
+  `xdebug` text,
+  `uri` text,
+  `request` text,
+  `ip` varchar(45) DEFAULT NULL,
+  `session_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
